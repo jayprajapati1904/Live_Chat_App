354f1fe1d6bbbc3d604a337775a159fc105fa09c
Jay <jay1904prajapati@gmail.com>
2024-11-26 15:29:26 +0530

    page layout is ready

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,7 +1,24 @@
+import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import SignInPage from "./pages/SignInPage";
+import ProfilePage from "./pages/ProfilePage";
+import SettingPage from "./pages/SettingPage";
+
 function App() {
   return (
     <>
-      <p className="text-red-500 bg-red-300 text-center">hello</p>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/signin" element={<SignInPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/setting" element={<SettingPage />} />
+        </Routes>
+      </div>
     </>
   );
 }
